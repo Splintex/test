55c78370da26422e905d2e0b6c87557c6a2b724b
Eugene Godun <eugene.godun@gmail.com>
2016-01-08 19:42:36 +0700

    fixes part 1

--- js/common.js
+++ js/common.js
@@ -1,9 +1,17 @@
-head.ready(function() {
-
+$(document).ready(function() {
+	$(document).on("click", function() {
+		$(".dropdown").removeClass("is-active");
+		$(".js-dropdown-search").removeClass("to-show");
+	});
 	$(".js-btn-dropdown").on("click", function(){
-		$(this).toggleClass("is-active");
-		$(".js-list-dropdown").toggleClass("is-active");
+		$(this).parents(".dropdown").toggleClass("is-active");
 		return false;
+	});
+	$(".dropdown").on("click", function(){
+		event.stopPropagation();
+	});
+	$(".js-dropdown-search").on("click", function(){
+		event.stopPropagation();
 	});
 	$(".js-raty").raty({
 		score: function() {
@@ -33,13 +41,6 @@ head.ready(function() {
 	$('.js-file-input').MultiFile({ 
 		list: '.js-files-list' 
 	});
-
-	// $(".js-file-input").on("change", function() {
-	//     for(var i = 0; i < this.files.length; i++) {
-	//     	$(this).parents(".to-comment").find(".js-files-list").append('<div class="download-name js-file-name"><span>'+this.files[i].name+'</span><button type="button"></button></div>');
-	//     }
-	//     $(".js-file-name").removeClass("is-inactive");
-	// });
 	$(".js-btn-close").on("click", function() {
 		$(".js-popup").removeClass("is-active");
 		$(".js-dropdown-search").removeClass("to-show");
@@ -64,5 +65,42 @@ head.ready(function() {
     $(".js-feedback").on("click", function(){
     	$(".js-popup-feedback").addClass("is-active");
     });
+
+    function tog(v) {
+    	return v?'addClass':'removeClass';
+    } 
+	$(document).on('input', '.js-input-search', function(){
+	    $(this)[tog(this.value)]('is-active');
+	});
+
+	$(".js-clear-search").on("click", function() {
+		$(this).parent().find("input").removeClass("is-active");
+	});
+
+	(function(){
+		var el = $(".js-sticky-add");
+		var parent = el.parent();
+		var top = parent.offset().top;
+		var left = parent.offset().left;
+		console.log(left);
+		$(window).resize(function() {
+			left = parent.offset().left;
+			el.css({
+				left: left
+			});
+		});
+		$(window).scroll(function() {
+			if($(window).scrollTop() + 20 >= top) {
+				el.addClass("is-fixed").css({
+					left: left
+				});
+			}
+			else {
+				el.removeClass("is-fixed");
+			}
+		});
+	})();
+	
+
 
 });
--- js/init.js
+++ js/init.js
@@ -1,5 +1,1 @@
-head.load("js/lib/jquery.js", 
-					"js/lib/cycle.js",
-					"js/lib/jquery.raty.js",
-					"js/lib/jquery.MultiFile.pack.js",
-					"js/common.js");
+
